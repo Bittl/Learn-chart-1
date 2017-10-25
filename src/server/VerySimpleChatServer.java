28b06fad96032c3541ebea65a57d02bb2a73fc29
Bittl <bittl100@gmail.com>
2017-10-25 07:11:49 +0300

    Do server

---
+++ src/server/VerySimpleChatServer.java
@@ -0,0 +1,81 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class VerySimpleChatServer {
+
+    ArrayList clientOutputStreams;
+
+    public class ClientHandler implements Runnable{
+
+        BufferedReader reader;
+        Socket socket;
+
+        public ClientHandler(Socket clientSocket){
+            try{
+                socket = clientSocket;
+                InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+                reader = new BufferedReader(streamReader);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            String message;
+            try{
+                while ((message = reader.readLine()) != null){
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new VerySimpleChatServer().go();
+    }
+
+    public void go(){
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            while (true){
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread thread = new Thread(new ClientHandler(clientSocket));
+                thread.start();
+                System.out.println("Got a connection");
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void tellEveryone(String message){
+        Iterator it = clientOutputStreams.iterator();
+        while (it.hasNext()){
+            try{
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);
+                writer.flush();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
